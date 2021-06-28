fdc2d7250c53ca1df89672d0008005c03284e39f
Gaurav Sharma <gaurav.sharma@punchh.com>
2021-06-28 20:27:32 +0530

    cmd fixes

--- commands/DbCreate.ts
+++ commands/DbCreate.ts
@@ -1,9 +1,7 @@
 import { BaseCommand } from '@adonisjs/core/build/standalone'
-import Env from 'env'
 import Knex from 'knex'
 
 export default class DbCreate extends BaseCommand {
-
   /**
    * Command name is used to run the command
    */
@@ -19,7 +17,7 @@ export default class DbCreate extends BaseCommand {
      * Set the following value to true, if you want to load the application
      * before running the command
      */
-    loadApp: false,
+    loadApp: true,
 
     /**
      * Set the following value to true, if you want this command to keep running until
@@ -27,14 +25,30 @@ export default class DbCreate extends BaseCommand {
      */
     stayAlive: false,
   }
-  info: any
 
-  public async run () {
-    const config = require(process.cwd() + '/config/database');
-    config[config.connection].connection.database = null;
-    const knex = Knex(config[config.connection]);
-    await knex.raw(`CREATE DATABASE IF NOT EXISTS ${Env.get('MYSQL_DB_NAME')}`);
-    await knex.destroy();
-    this.info('DB created');
+  public async run() {
+    try {
+      const { default: env } = await import('@ioc:Adonis/Core/Env')
+      const { default: config } = await import('@ioc:Adonis/Core/Config')
+      const connName = env.get('DB_CONNECTION')
+      const conn = config.get('database.connections')[connName]
+      conn.connection.database = null
+      const knex = Knex(conn)
+      await knex.raw(`CREATE DATABASE ${env.get(`${connName.toUpperCase()}_DB_NAME`)}`)
+      await knex.destroy()
+    } catch (e) {
+      if (e.code === 'ER_DB_CREATE_EXISTS') {
+        this.logger.error('DB already exist')
+      } else {
+        this.logger.error(e)
+      }
+      return
+    }
+    this.logger.info('DB created')
+
+    // const config = require(process.cwd() + '/config/database');
+    // config[config.connection].connection.database = null;
+    // const knex = Knex(config[config.connection]);
+    // await knex.raw(`CREATE DATABASE IF NOT EXISTS ${Env.get('MYSQL_DB_NAME')}`);
   }
 }
--- commands/DbDrop.ts
+++ commands/DbDrop.ts
@@ -1,9 +1,7 @@
 import { BaseCommand } from '@adonisjs/core/build/standalone'
-import Env from 'env'
 import Knex from 'knex'
 
-export default class DbDrop extends BaseCommand {
-
+export default class DbDrop11 extends BaseCommand {
   /**
    * Command name is used to run the command
    */
@@ -19,7 +17,7 @@ export default class DbDrop extends BaseCommand {
      * Set the following value to true, if you want to load the application
      * before running the command
      */
-    loadApp: false,
+    loadApp: true,
 
     /**
      * Set the following value to true, if you want this command to keep running until
@@ -28,11 +26,22 @@ export default class DbDrop extends BaseCommand {
     stayAlive: false,
   }
 
-  public async run () {
-    const config = require(process.cwd() + '/config/database');
-    const knex = Knex(config[config.connection]);
-    await knex.raw(`DROP DATABASE ${Env.get("DB_DATABASE")}`);
-    await knex.destroy();
-    this.info('DB dropped');
+  public async run() {
+    try {
+      const { default: env } = await import('@ioc:Adonis/Core/Env')
+      const { default: config } = await import('@ioc:Adonis/Core/Config')
+      const connName = env.get('DB_CONNECTION')
+      const knex = Knex(config.get('database.connections')[connName])
+      await knex.raw(`DROP DATABASE IF EXISTS ${env.get(`${connName.toUpperCase()}_DB_NAME`)}`)
+      await knex.destroy()
+    } catch (e) {
+      if (e.code === 'ER_BAD_DB_ERROR') {
+        this.logger.error("DB doesn't exist")
+      } else {
+        this.logger.error(e)
+      }
+      return
+    }
+    this.logger.info('DB dropped')
   }
 }
