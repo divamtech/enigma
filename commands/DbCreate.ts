6d17bc122aac2e97cb70b1dcc3b97b9aa413e77d
Gaurav Sharma <gaurav.sharma@punchh.com>
2021-06-28 20:49:21 +0530

    minor changes

--- commands/DbCreate.ts
+++ commands/DbCreate.ts
@@ -27,28 +27,23 @@ export default class DbCreate extends BaseCommand {
   }
 
   public async run() {
+    const { default: env } = await import('@ioc:Adonis/Core/Env')
+    const { default: config } = await import('@ioc:Adonis/Core/Config')
+    const connName = env.get('DB_CONNECTION')
+    const conn = config.get('database.connections')[connName]
+    const dbName = env.get(`${connName.toUpperCase()}_DB_NAME`)
     try {
-      const { default: env } = await import('@ioc:Adonis/Core/Env')
-      const { default: config } = await import('@ioc:Adonis/Core/Config')
-      const connName = env.get('DB_CONNECTION')
-      const conn = config.get('database.connections')[connName]
       conn.connection.database = null
       const knex = Knex(conn)
-      await knex.raw(`CREATE DATABASE ${env.get(`${connName.toUpperCase()}_DB_NAME`)}`)
+      await knex.raw(`CREATE DATABASE ${dbName}`)
       await knex.destroy()
+      this.logger.info(`DB (${dbName}) created`)
     } catch (e) {
       if (e.code === 'ER_DB_CREATE_EXISTS') {
-        this.logger.error('DB already exist')
+        this.logger.error(`DB (${dbName}) already exist`)
       } else {
         this.logger.error(e)
       }
-      return
     }
-    this.logger.info('DB created')
-
-    // const config = require(process.cwd() + '/config/database');
-    // config[config.connection].connection.database = null;
-    // const knex = Knex(config[config.connection]);
-    // await knex.raw(`CREATE DATABASE IF NOT EXISTS ${Env.get('MYSQL_DB_NAME')}`);
   }
 }
--- commands/DbDrop.ts
+++ commands/DbDrop.ts
@@ -27,21 +27,22 @@ export default class DbDrop11 extends BaseCommand {
   }
 
   public async run() {
+    const { default: env } = await import('@ioc:Adonis/Core/Env')
+    const { default: config } = await import('@ioc:Adonis/Core/Config')
+    const connName = env.get('DB_CONNECTION')
+    const conn = config.get('database.connections')[connName]
+    const dbName = env.get(`${connName.toUpperCase()}_DB_NAME`)
     try {
-      const { default: env } = await import('@ioc:Adonis/Core/Env')
-      const { default: config } = await import('@ioc:Adonis/Core/Config')
-      const connName = env.get('DB_CONNECTION')
-      const knex = Knex(config.get('database.connections')[connName])
-      await knex.raw(`DROP DATABASE IF EXISTS ${env.get(`${connName.toUpperCase()}_DB_NAME`)}`)
+      const knex = Knex(conn)
+      await knex.raw(`DROP DATABASE ${dbName}`)
       await knex.destroy()
+      this.logger.info(`DB (${dbName}) dropped`)
     } catch (e) {
       if (e.code === 'ER_BAD_DB_ERROR') {
-        this.logger.error("DB doesn't exist")
+        this.logger.error(`DB (${dbName}) doesn't exist`)
       } else {
         this.logger.error(e)
       }
-      return
     }
-    this.logger.info('DB dropped')
   }
 }
